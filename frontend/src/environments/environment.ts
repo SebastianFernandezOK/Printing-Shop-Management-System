68ae4bbaf555b131ca32e2feae24ace136f289e1
SebastianFernandezOK <sebastianfernandez2812@gmail.com>
2025-11-02 22:16:47 -0300

    docker

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 // Development environment configuration
 export const environment = {
   production: false,
-  apiUrl: 'https://34.136.188.79/'  // Backend API base URL for development
+  apiUrl: 'https://34.136.188.79:9000'  // Backend API base URL for development
 };
